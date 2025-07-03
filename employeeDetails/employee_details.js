70bb571a2dfcb1ded9b5e09c06ed5bf2069c6914
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2025-07-03 07:38:00 +0000

    Add function to find employee by ID and display details

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -21,3 +21,13 @@ function displayHREmployees() {
       const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
       document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
+
+function findEmployeeById(employeeId) {
+      const foundEmployee = employees.find(employee => employee.id === employeeId);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
